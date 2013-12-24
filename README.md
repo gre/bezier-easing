cc463d23d19dbb644cd2442683c4383ef81b5e51
Gaëtan Renaudeau <gre@zenexity.com>
2013-12-24 18:57:55 +0100

    v0.2.1 : supports both requirejs / node require() / standalone
    + Add example
    + Add more info in README

--- README.md
+++ README.md
@@ -1,4 +1,73 @@
 bezier-easing
 ===
 
+BezierEasing provides interpolation to make Bezier Curve based easing functions for your JavaScript animations.
+
+It is the equivalent to [CSS Transitions' `transition-timing-function`](http://www.w3.org/TR/css3-transitions/#transition-timing-function-property).
+
+See this schema from the CSS spec:
+
+[![TimingFunction.png](http://www.w3.org/TR/css3-transitions/TimingFunction.png)](http://www.w3.org/TR/css3-transitions/#transition-timing-function-property)
+
+In CSS you can define easing with `cubic-bezier(0.42, 0, 0.58, 1)`, 
+with BezierEasing, you can define it using `BezierEasing(0.42, 0, 0.58, 1)` which retuns a function taking an X and computing the Y interpolated easing value (see the schema).
+
+**Example:**
+
+[See it for real here](http://greweb.me/bezier-easing/example).
+
+```javscript
+var canvas = document.getElementById("viewport"), ctx = canvas.getContext("2d");
+
+animate(moveRectangle, 2000, BezierEasing(0.25, 0.1, 0.0, 1.0));
+
+function moveRectangle (p) { // p move from 0 to 1
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.fillStyle = "hsl("+Math.round(255*p)+",80%,50%)";
+  var w = 50;
+  var h = 50 + p * (canvas.height - 50);
+  ctx.fillRect((canvas.width-w) * p, (canvas.height-h)*0.5, w, h);
+}
+
+function animate (render, duration, easing) {
+  var start = Date.now();
+  (function loop () {
+    var p = (Date.now()-start)/duration;
+    if (p > 1) {
+      render(1);
+    }
+    else {
+      requestAnimationFrame(loop);
+      render(easing(p));
+    }
+  }());
+}
+```
+
+Predefined BezierEasing functions
+---
+
+We have defined for you all existing CSS `transition-timing-function` :
+
+```javscript
+BezierEasing.css = {
+  "ease":        BezierEasing(0.25, 0.1, 0.25, 1.0), 
+  "linear":      BezierEasing(0.00, 0.0, 1.00, 1.0),
+  "ease-in":     BezierEasing(0.42, 0.0, 1.00, 1.0),
+  "ease-out":    BezierEasing(0.00, 0.0, 0.58, 1.0),
+  "ease-in-out": BezierEasing(0.42, 0.0, 0.58, 1.0)
+};
+```
+
+Perfect if you want for instance to make an abstraction on top of CSS and JavaScript animations.
+
+Who use it?
+===
+
+- [Apple®](http://images.apple.com/v/mac-pro/home/b/scripts/overview.js) :)
+
+More informations
+===
+
 Implementation based on this [article](http://greweb.me/2012/02/bezier-curve-based-easing-functions-from-concept-to-implementation/).
+
--- index.js
+++ index.js
@@ -1,53 +1,65 @@
+(function (definition) {
+  if (typeof exports === "object") {
+    module.exports = definition();
+  }
+  else if (typeof window.define === 'function' && window.define.amd) {
+    window.define([], definition);
+  } else {
+    window.BezierEasing = definition();
+  }
+}(function () {
 
-/**
-* BezierEasing - use bezier curve for transition easing function
-* is inspired from Firefox's nsSMILKeySpline.cpp
-* Usage:
-* var spline = new BezierEasing(0.25, 0.1, 0.25, 1.0)
-* spline(x) => returns the easing value | x must be in [0, 1] range
-*/
-function BezierEasing (mX1, mY1, mX2, mY2) {
-  if (!(this instanceof BezierEasing)) return new BezierEasing(mX1, mY1, mX2, mY2);
- 
-  function A(aA1, aA2) { return 1.0 - 3.0 * aA2 + 3.0 * aA1; }
-  function B(aA1, aA2) { return 3.0 * aA2 - 6.0 * aA1; }
-  function C(aA1)      { return 3.0 * aA1; }
- 
-  // Returns x(t) given t, x1, and x2, or y(t) given t, y1, and y2.
-  function CalcBezier(aT, aA1, aA2) {
-    return ((A(aA1, aA2)*aT + B(aA1, aA2))*aT + C(aA1))*aT;
-  }
- 
-  // Returns dx/dt given t, x1, and x2, or dy/dt given t, y1, and y2.
-  function GetSlope(aT, aA1, aA2) {
-    return 3.0 * A(aA1, aA2)*aT*aT + 2.0 * B(aA1, aA2) * aT + C(aA1);
-  }
- 
-  function GetTForX(aX) {
-    // Newton raphson iteration
-    var aGuessT = aX;
-    for (var i = 0; i < 4; ++i) {
-      var currentSlope = GetSlope(aGuessT, mX1, mX2);
-      if (currentSlope === 0.0) return aGuessT;
-      var currentX = CalcBezier(aGuessT, mX1, mX2) - aX;
-      aGuessT -= currentX / currentSlope;
+  /**
+  * BezierEasing - use bezier curve for transition easing function
+  * is inspired from Firefox's nsSMILKeySpline.cpp
+  * Usage:
+  * var spline = new BezierEasing(0.25, 0.1, 0.25, 1.0)
+  * spline(x) => returns the easing value | x must be in [0, 1] range
+  */
+  function BezierEasing (mX1, mY1, mX2, mY2) {
+    if (!(this instanceof BezierEasing)) return new BezierEasing(mX1, mY1, mX2, mY2);
+   
+    function A(aA1, aA2) { return 1.0 - 3.0 * aA2 + 3.0 * aA1; }
+    function B(aA1, aA2) { return 3.0 * aA2 - 6.0 * aA1; }
+    function C(aA1)      { return 3.0 * aA1; }
+   
+    // Returns x(t) given t, x1, and x2, or y(t) given t, y1, and y2.
+    function CalcBezier(aT, aA1, aA2) {
+      return ((A(aA1, aA2)*aT + B(aA1, aA2))*aT + C(aA1))*aT;
     }
-    return aGuessT;
+   
+    // Returns dx/dt given t, x1, and x2, or dy/dt given t, y1, and y2.
+    function GetSlope(aT, aA1, aA2) {
+      return 3.0 * A(aA1, aA2)*aT*aT + 2.0 * B(aA1, aA2) * aT + C(aA1);
+    }
+   
+    function GetTForX(aX) {
+      // Newton raphson iteration
+      var aGuessT = aX;
+      for (var i = 0; i < 4; ++i) {
+        var currentSlope = GetSlope(aGuessT, mX1, mX2);
+        if (currentSlope === 0.0) return aGuessT;
+        var currentX = CalcBezier(aGuessT, mX1, mX2) - aX;
+        aGuessT -= currentX / currentSlope;
+      }
+      return aGuessT;
+    }
+
+    return function (aX) {
+      if (mX1 === mY1 && mX2 === mY2) return aX; // linear
+      return CalcBezier(GetTForX(aX), mY1, mY2);
+    };
   }
 
-  return function (aX) {
-    if (mX1 === mY1 && mX2 === mY2) return aX; // linear
-    return CalcBezier(GetTForX(aX), mY1, mY2);
-  };
-}
-
-// CSS mapping
-BezierEasing.css = {
+  // CSS mapping
+  BezierEasing.css = {
     "ease":        BezierEasing(0.25, 0.1, 0.25, 1.0), 
     "linear":      BezierEasing(0.00, 0.0, 1.00, 1.0),
     "ease-in":     BezierEasing(0.42, 0.0, 1.00, 1.0),
     "ease-out":    BezierEasing(0.00, 0.0, 0.58, 1.0),
     "ease-in-out": BezierEasing(0.42, 0.0, 0.58, 1.0)
-};
+  };
 
-module.exports = BezierEasing;
+  return BezierEasing;
+
+}));
