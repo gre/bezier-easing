acef1e2b4d29fc3cbb41570ba1f5d7ea9613e1d6
Gaëtan Renaudeau <renaudeau.gaetan@gmail.com>
2015-06-20 10:38:43 +0200

    improve benchmark and add visual-demo

--- benchmark.js
+++ benchmark.js
@@ -8,27 +8,23 @@ var random = Math.random;
 var bezier = BezierEasing(0.2, 0.3, 1.0, 0.5);
 
 suite
-.add('BezierEasing: instanciations', function() {
-  for (var i=0; i<1000; i++) {
-    BezierEasing(random(), random(), random(), random());
-  }
+.add('BezierEasing: instanciation', function() {
+  BezierEasing(random(), random(), random(), random());
 })
-.add('BezierEasing: calls', function() {
-  for (var i=0; i<1000; i++) {
-    bezier(random());
-  }
+.add('BezierEasing: call', function() {
+  bezier(random());
 })
-.add('BezierEasing: instanciations & calls', function() {
-  for (var i=0; i<100; i++) {
-    var bezier = BezierEasing(random(), random(), random(), random());
-    for (var j=0; j<10; j++) {
-      bezier(random());
-    }
-  }
+.add('BezierEasing: instanciation + call', function() {
+  var bezier = BezierEasing(random(), random(), random(), random());
+  bezier(random());
 })
 // add listeners
 .on('cycle', function(event) {
-  console.log(String(event.target));
+  var log = String(event.target);
+  if (typeof document !== "undefined")
+    document.body.innerHTML += "<p><code>"+log+"</code></p>";
+  else
+    console.log(log);
 })
 // run async
 .run();
